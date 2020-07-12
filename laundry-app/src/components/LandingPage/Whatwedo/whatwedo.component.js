cf3eea24389b479639b92acce58ab4200ee13404
xnoblejoy <57623521+xnoblejoy@users.noreply.github.com>
2020-07-12 20:13:56 +0530

    Added Map to landing page footer

--- laundry-app/src/components/LandingPage/Whatwedo/whatwedo.component.js
+++ laundry-app/src/components/LandingPage/Whatwedo/whatwedo.component.js
@@ -125,6 +125,24 @@ class WhatwedoComponent extends Component {
                         <div className="clearfix"></div>
                     </div>
                 </section>
+                <div class="contact" id="contact">
+                    <div class="container">
+                        <div class="agile-contact-grids">
+                            <h3>Contact Us</h3>
+                            <div class="col-lg-12 agile-contact-left">
+                                <div class="agileits-map">
+
+                                </div>
+                                <div class="map-grid">
+                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3945.6717134527485!2d76.8850869147831!3d8.531217193867148!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b05be9baab0b293%3A0x8ac4bd082d5b0dd8!2sAishwarya%20Homes%20Infinity!5e0!3m2!1sen!2sin!4v1594563561355!5m2!1sen!2sin" width="1100" height="500" frameborder="0" style={{ border: 0 }} allowfullscreen="" aria-hidden="false" tabindex="0" title="laundry"></iframe>
+                                </div>
+                            </div>
+
+
+                            <div class="clearfix"> </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
